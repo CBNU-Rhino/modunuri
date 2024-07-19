c3dbc6ac9cf969203d2f3a6ea1a0631177fc1ad7
yangwoohyeon <dngus0717@gmail.com>
2024-07-19 22:27:18 +0900

    User엔티티

--- src/main/java/app/app/domain/User.java
+++ src/main/java/app/app/domain/User.java
@@ -1,4 +1,4 @@
-package app.app.user;
+package app.app.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -9,27 +9,32 @@ import lombok.NoArgsConstructor;
 @Entity(name= "user")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class User{
+public class User extends BaseTimeEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = "id") //@Colum == 내부변수 정의
     private Long id;
 
     @Column(name = "username")
     private String username;
+
     @Column(name = "password")
     private String password;
 
-    @Builder
-    public User(String username, String password) {
+    @Column(name ="Email")
+    private String email;
+
+    @Builder //
+    public User(String username, String password, String email) {
         this.username = username;
         this.password = password;
+        this.email=email;
     }
 
     //편의 메서드
-    public void update(String username, String password) {
+    public void update(String username, String password, String email) {
         this.username=username;
         this.password = password;
-
+        this.email = email;
     }
 }
